bda98e64b1c2777b3d3a21d3359a7e94be575679
T145 <T145@protonmail.com>
2024-12-22 11:24:44 -0500

    Code formatting (chore)

--- spectrum.py
+++ spectrum.py
@@ -11,7 +11,6 @@ from transformers import AutoConfig, AutoModelForCausalLM
 
 
 class ModelModifier:
-
     def __init__(self, model_name=None, top_percent=50, batch_size=1, cuda=False):
         self.model_name = model_name
         self.top_percent = top_percent
@@ -26,14 +25,14 @@ class ModelModifier:
                         torch_dtype=torch.float32,
                         low_cpu_mem_usage=True,
                         trust_remote_code=True,
-                        device_map="auto"
+                        device_map="auto",
                     )
                 else:
                     self.model = AutoModelForCausalLM.from_pretrained(
                         model_name,
                         torch_dtype=torch.float32,
                         trust_remote_code=True,
-                        device_map="cpu"
+                        device_map="cpu",
                     )
             except KeyError as e:
                 print(f"Error loading model: {e}")
@@ -47,38 +46,38 @@ class ModelModifier:
                     torch_dtype=torch.float32,
                     low_cpu_mem_usage=True,
                     trust_remote_code=True,
-                    device_map="auto"
+                    device_map="auto",
                 )
-            except (RuntimeError or NotImplementedError) as e:
+            except RuntimeError or NotImplementedError as e:
                 print(f"Error loading model: {e}")
                 print("Try passing --cuda=False!")
 
             # Check if the model config has rope_scaling
-            if not hasattr(self.model.config, 'rope_scaling'):
-                self.model.config.rope_scaling = {'type': 'linear'}
+            if not hasattr(self.model.config, "rope_scaling"):
+                self.model.config.rope_scaling = {"type": "linear"}
             elif not isinstance(self.model.config.rope_scaling, dict):
-                self.model.config.rope_scaling = {'type': 'linear'}
-            elif 'type' not in self.model.config.rope_scaling:
-                self.model.config.rope_scaling['type'] = 'linear'
+                self.model.config.rope_scaling = {"type": "linear"}
+            elif "type" not in self.model.config.rope_scaling:
+                self.model.config.rope_scaling["type"] = "linear"
         else:
             self.model = None
 
         self.layer_snr = dict()
 
-
     def get_weight_types(self):
         weight_types = set()
 
         for name, module in self.model.named_modules():
-            parts = name.split('.')
+            parts = name.split(".")
 
-            if any(hasattr(module, attr) for attr in ['weight', 'bias', 'inv_freq']):
-                layer_index = next((i for i, part in enumerate(parts) if part.isdigit()), -1)
-                weight_type = '.'.join(parts[layer_index + 1:]) if layer_index != -1 else name
+            if any(hasattr(module, attr) for attr in ["weight", "bias", "inv_freq"]):
+                layer_index = next(
+                    (i for i, part in enumerate(parts) if part.isdigit()), -1
+                )
+                weight_type = (".".join(parts[layer_index + 1 :]) if layer_index != -1 else name)
                 weight_types.add(weight_type)
 
         return list(weight_types)
-
 
     def select_weights(self):
         weight_types = self.get_weight_types()
@@ -86,24 +85,34 @@ class ModelModifier:
 
         for wt in weight_types:
             print(wt)
-            category, layer = wt.split('.')
+            category, layer = wt.split(".")
 
             if category in ["mlp", "self_attn"] and layer != "rotary_emb":
                 categories.setdefault(category, list()).append(wt)
 
-        sorted_categories = {k: sorted(v) for k, v in sorted(categories.items(), key=lambda item: item[0])}
-        sorted_weight_types = [wt for sublist in sorted_categories.values() for wt in sublist]
+        sorted_categories = {
+            k: sorted(v)
+            for k, v in sorted(categories.items(), key=lambda item: item[0])
+        }
+        sorted_weight_types = [
+            wt for sublist in sorted_categories.values() for wt in sublist
+        ]
 
         return sorted_weight_types
 
-
     def calculate_snr_for_layer(self, layer_type):
-        layers = [(name, module) for name, module in self.model.named_modules() if layer_type in name and hasattr(module, 'weight')]
+        layers = [
+            (name, module)
+            for name, module in self.model.named_modules()
+            if layer_type in name and hasattr(module, "weight")
+        ]
         num_batches = (len(layers) + self.batch_size - 1) // self.batch_size
 
-        with tqdm(total=num_batches, unit='batch', desc=f'Calculating SNR for {layer_type}') as progress_bar:
+        with tqdm(
+            total=num_batches, unit="batch", desc=f"Calculating SNR for {layer_type}"
+        ) as progress_bar:
             for i in range(0, len(layers), self.batch_size):
-                batch_layers = layers[i:i + self.batch_size]
+                batch_layers = layers[i : i + self.batch_size]
 
                 for name, module in batch_layers:
                     weights = module.weight.detach()
@@ -118,19 +127,17 @@ class ModelModifier:
                     mp_threshold = self.marchenko_pastur_threshold(sigma_estimated, n, m)
                     signal = S[S > mp_threshold].sum()
                     noise = S[S <= mp_threshold].sum()
-                    snr = signal / noise if noise != 0 else float('inf')
+                    snr = signal / noise if noise != 0 else float("inf")
                     snr_ratio = snr / max_singular_value
-                    self.layer_snr[name] = {'type': layer_type, 'snr': snr_ratio.item()}
+                    self.layer_snr[name] = {"type": layer_type, "snr": snr_ratio.item()}
 
                 progress_bar.update(1)
-
 
     @staticmethod
     def marchenko_pastur_threshold(sigma, n, m):
         beta = n / m if n < m else m / n
         threshold = sigma * np.sqrt((1 + np.sqrt(beta)) ** 2)
         return threshold
-
 
     @staticmethod
     def estimate_sigma_with_full_iqr(S):
@@ -140,12 +147,15 @@ class ModelModifier:
         sigma_estimated = iqr / 1.349
         return sigma_estimated
 
-
     def assess_layers_snr(self, selected_weight_types):
         # total_layers = sum(1 for name, module in self.model.named_modules() if any(layer_type in name for layer_type in selected_weight_types) and hasattr(module, 'weight'))
         start_time = time.time()
 
-        with tqdm(total=len(selected_weight_types), unit='type', desc='Calculating SNR for types') as progress_bar:
+        with tqdm(
+            total=len(selected_weight_types),
+            unit="type",
+            desc="Calculating SNR for types",
+        ) as progress_bar:
             for layer_type in selected_weight_types:
                 self.calculate_snr_for_layer(layer_type)
                 progress_bar.update(1)
@@ -154,57 +164,65 @@ class ModelModifier:
         total_time = end_time - start_time
         print(f"Total time taken: {total_time:.2f} seconds")
 
-
     def save_snr_to_json(self):
-        model_name_slug = os.path.basename(self.model_name) if os.path.exists(self.model_name) else self.model_name.replace('/', '-').replace('_', '-')
-        directory = 'model_snr_results'
-        filename = os.path.join(directory, f'snr_results_{model_name_slug}.json')
+        model_name_slug = (
+            os.path.basename(self.model_name)
+            if os.path.exists(self.model_name)
+            else self.model_name.replace("/", "-").replace("_", "-")
+        )
+        directory = "model_snr_results"
+        filename = os.path.join(directory, f"snr_results_{model_name_slug}.json")
 
         os.makedirs(directory, exist_ok=True)
 
         serializable_data = dict()
 
         for layer_name, info in self.layer_snr.items():
-            snr_value = info['snr'].item() if isinstance(info['snr'], torch.Tensor) else info['snr']
-            layer_type = str(info['type'])
-            serializable_data[layer_name] = {'snr': snr_value, 'type': layer_type}
+            snr_value = (
+                info["snr"].item()
+                if isinstance(info["snr"], torch.Tensor)
+                else info["snr"]
+            )
+            layer_type = str(info["type"])
+            serializable_data[layer_name] = {"snr": snr_value, "type": layer_type}
 
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             json.dump(serializable_data, file, indent=4)
 
         print(f"Results saved to {filename}")
         self.save_top_snr_ratios_to_json(filename)
         self.generate_unfrozen_params_yaml(filename)
 
-
     def generate_unfrozen_params_yaml(self, json_filename, top_percent=None):
         top_percent = top_percent if top_percent is not None else self.top_percent
 
-        with open(json_filename, 'r') as file:
+        with open(json_filename, "r") as file:
             snr_data = json.load(file)
 
         unfrozen_parameters = dict()
 
         for layer_name, info in snr_data.items():
-            layer_type = info['type']
+            layer_type = info["type"]
 
             if layer_type not in unfrozen_parameters:
                 unfrozen_parameters[layer_type] = list()
 
-            unfrozen_parameters[layer_type].append((layer_name, info['snr']))
+            unfrozen_parameters[layer_type].append((layer_name, info["snr"]))
 
         top_layers_by_type = dict()
 
         for layer_type, layers in unfrozen_parameters.items():
             layers_sorted = sorted(layers, key=lambda x: x[1], reverse=True)
             num_top_layers = int(len(layers) * top_percent / 100)
-            top_layers_by_type[layer_type] = [layer[0] for layer in layers_sorted[:num_top_layers]]
+            top_layers_by_type[layer_type] = [
+                layer[0] for layer in layers_sorted[:num_top_layers]
+            ]
 
         # Modify the yaml_filename to include the input json name and top_percent
         json_file_base = os.path.splitext(os.path.basename(json_filename))[0]
         yaml_filename = f"{json_file_base}_unfrozenparameters_{top_percent}percent.yaml"
 
-        with open(yaml_filename, 'w') as file:
+        with open(yaml_filename, "w") as file:
             file.write("unfrozen_parameters:\n")
             file.write("- ^lm_head.weight$\n")
             file.write("- ^model.embed_tokens.weight$\n")
@@ -217,20 +235,19 @@ class ModelModifier:
 
         print(f"Top {top_percent}% SNR layers saved to {yaml_filename}")
 
-
     def save_top_snr_ratios_to_json(self, json_filename, filename=None):
-        with open(json_filename, 'r') as file:
+        with open(json_filename, "r") as file:
             snr_data = json.load(file)
 
         all_snr_layers = dict()
 
         for layer_name, info in snr_data.items():
-            layer_type = info['type']
+            layer_type = info["type"]
 
             if layer_type not in all_snr_layers:
                 all_snr_layers[layer_type] = list()
 
-            all_snr_layers[layer_type].append((layer_name, info['snr']))
+            all_snr_layers[layer_type].append((layer_name, info["snr"]))
 
         for layer_type, layers in all_snr_layers.items():
             layers_sorted = sorted(layers, key=lambda x: x[1], reverse=True)
@@ -239,7 +256,7 @@ class ModelModifier:
         json_file_base = os.path.splitext(os.path.basename(json_filename))[0]
         filename = f"{json_file_base}_sorted.json" if filename is None else filename
 
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             json.dump(all_snr_layers, file, indent=4)
 
         print(f"All SNR layers sorted and saved to {filename}")
@@ -248,15 +265,15 @@ class ModelModifier:
 def main():
     # Handle command-line arguments
     parser = argparse.ArgumentParser(description="Process SNR data for layers.")
-    parser.add_argument('--model-name', type=str, required=True, help='Model name or path to the model')
-    parser.add_argument('--top-percent', type=int, default=50, help='Top percentage of layers to select')
-    parser.add_argument('--batch-size', type=int, default=1, help='Job batch size')
-    parser.add_argument('--cuda', type=bool, default=False, help='Whether to use the GPU')
+    parser.add_argument("--model-name", type=str, required=True, help="Model name or path to the model")
+    parser.add_argument("--top-percent", type=int, default=50, help="Top percentage of layers to select")
+    parser.add_argument("--batch-size", type=int, default=1, help="Job batch size")
+    parser.add_argument("--cuda", type=bool, default=False, help="Whether to use the GPU")
     args = parser.parse_args()
 
     # Check for existing SNR results file
-    model_name_slug = args.model_name.replace('/', '-').replace('_', '-')
-    snr_file_path = os.path.join('model_snr_results', f'snr_results_{model_name_slug}.json')
+    model_name_slug = args.model_name.replace("/", "-").replace("_", "-")
+    snr_file_path = os.path.join("model_snr_results", f"snr_results_{model_name_slug}.json")
 
     if os.path.exists(snr_file_path):
         print(f"Found existing SNR results file for {args.model_name}")
